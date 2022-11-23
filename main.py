f06defa91b469c7a4110a8cade14592f90e070d2
arthurjamesoncode <arthurj17@gmail.com>
2022-11-23 18:03:48 +0000

    refactor
    just cleaned up code to make it more readable and organised

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ Builder.load_file("menu.kv")
 
 class MainWidget(RelativeLayout):
     from transforms import transform, transform_2D, transform_perspective
-    from user_actions import _keyboard_closed, on_keyboard_down, on_keyboard_up, on_touch_down, on_touch_up, on_touch_move
+    from user_actions import _keyboard_closed, on_keyboard_down, on_keyboard_up, on_touch_down, on_touch_up, on_touch_move, adjust_x_speed
 
     menu_widget = ObjectProperty()
     perspective_point_x = NumericProperty(0)
@@ -61,17 +61,15 @@ class MainWidget(RelativeLayout):
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
 
+        self.init_audio()
         self.init_vertical_lines()
         self.init_horizontal_lines()
         self.init_tiles()
-        self.reset_game()
         self.init_ship()
-        self.init_audio()
+        self.reset_game()
 
         if self.is_desktop():
-            self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
-            self._keyboard.bind(on_key_down=self.on_keyboard_down)
-            self._keyboard.bind(on_key_up=self.on_keyboard_up)
+            self.init_keyboard_controls()
 
         Clock.schedule_interval(self.update, 1/60)
         self.sound_galaxy.play()
@@ -90,6 +88,32 @@ class MainWidget(RelativeLayout):
         self.sound_game_over_voice.volume = 0.25
         self.sound_restart.volume = 0.25
         self.sound_game_over_impact.volume = 0.25
+    
+    def init_vertical_lines(self):
+        with self.canvas:
+            Color(1, 1, 1)
+
+            for i in range(0, self.V_NB_LINES):
+                self.vertical_lines.append(Line(points=[]))
+
+    def init_horizontal_lines(self):
+        with self.canvas:
+            Color(1, 1, 1)
+
+            for i in range(0, self.H_NB_LINES):
+                self.horizontal_lines.append(Line(points=[]))
+
+    def init_tiles(self):
+        with self.canvas:
+            Color(1, 1, 1)
+
+            for i in range(0, self.NB_TILES):
+                self.tiles.append(Quad())
+
+    def init_ship(self):
+        with self.canvas:
+            Color(0, 0, 0)
+            self.ship = Triangle()
 
     def reset_game(self):
         self.current_offset_y = 0
@@ -110,14 +134,12 @@ class MainWidget(RelativeLayout):
         if platform in ("linux", "win", "macosx"):
             return True
         return False 
-
-    def init_vertical_lines(self):
-        with self.canvas:
-            Color(1, 1, 1)
-
-            for i in range(0, self.V_NB_LINES):
-                self.vertical_lines.append(Line(points=[]))
     
+    def init_keyboard_controls(self):
+        self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
+        self._keyboard.bind(on_key_down=self.on_keyboard_down)
+        self._keyboard.bind(on_key_up=self.on_keyboard_up)
+
     def get_line_x_from_index(self, index):
         center_x = self.perspective_point_x
         spacing_x = self.V_LINES_SPACING * self.width
@@ -126,51 +148,12 @@ class MainWidget(RelativeLayout):
         line_x = center_x + offset * spacing_x + self.current_offset_x
         return line_x
     
-    def update_vertical_lines(self):
-        start_index = -int(self.V_NB_LINES/2)+1
-        end_index = start_index + self.V_NB_LINES
-
-        for i in range(start_index, end_index):
-            line_x = self.get_line_x_from_index(i)
-
-            x1, y1 = self.transform(line_x, 0)
-            x2, y2 = self.transform(line_x, self.height)
-            self.vertical_lines[i].points = [x1,y1, x2,y2]
-    
-    def init_horizontal_lines(self):
-        with self.canvas:
-            Color(1, 1, 1)
-
-            for i in range(0, self.H_NB_LINES):
-                self.horizontal_lines.append(Line(points=[]))
-    
     def get_line_y_from_index(self, index):
         spacing_y = self.H_LINES_SPACING * self.height
         offset = index
 
         line_y = spacing_y * offset - self.current_offset_y
         return line_y
-
-    def update_horizontal_lines(self):
-        start_index = -int(self.V_NB_LINES/2) +1
-        end_index = start_index + self.V_NB_LINES-1
-        
-        min_x = self.get_line_x_from_index(start_index)
-        max_x = self.get_line_x_from_index(end_index)
-
-        for i in range(0, self.H_NB_LINES):
-            line_y = self.get_line_y_from_index(i) 
-
-            x1, y1 = self.transform(min_x, line_y)
-            x2, y2 = self.transform(max_x, line_y)
-            self.horizontal_lines[i].points = [x1,y1, x2,y2]  
-
-    def init_tiles(self):
-        with self.canvas:
-            Color(1, 1, 1)
-
-            for i in range(0, self.NB_TILES):
-                self.tiles.append(Quad())
 
     def pre_fill_tiles_coordinates(self):
         for i in range(10):
@@ -220,42 +203,6 @@ class MainWidget(RelativeLayout):
         last_y = last_coordinates[1]
         return last_x, last_y
     
-    def update_tiles(self):
-        
-        for i in range(0, self.NB_TILES):
-            coordinates = self.tiles_coordinates[i]
-            min_x, min_y = self.get_tile_cordinate(coordinates[0], coordinates[1])
-            max_x, max_y = self.get_tile_cordinate(coordinates[0]+1, coordinates[1]+1)
-
-            x1, y1 = self.transform(min_x, min_y)
-            x2, y2 = self.transform(min_x, max_y)
-            x3, y3 = self.transform(max_x, max_y)
-            x4, y4 = self.transform(max_x, min_y)
-
-            self.tiles[i].points = [x1,y1, x2,y2, x3,y3, x4,y4]
-    
-    def init_ship(self):
-        with self.canvas:
-            Color(0, 0, 0)
-            self.ship = Triangle()
-    
-    def update_ship(self):
-        center_x = int(self.width/2)
-        ship_width = self.SHIP_WIDTH*self.width
-
-        ship_height = self.SHIP_HEIGHT * self.height
-        ship_base_y = self.SHIP_BASE_Y * self.height
-
-        self.ship_coordinates[0] = (int(center_x - ship_width/2), ship_base_y)
-        self.ship_coordinates[1] = (center_x, ship_base_y + ship_height)
-        self.ship_coordinates[2] = (int(center_x + ship_width/2), ship_base_y)
-
-        x1, y1 = self.transform(*self.ship_coordinates[0])
-        x2, y2 = self.transform(*self.ship_coordinates[1])
-        x3, y3 = self.transform(*self.ship_coordinates[2])
-
-        self.ship.points = [x1,y1, x2,y2, x3,y3]
-
     def check_ship_collision_with_tile(self, ti_x, ti_y):
         min_x, min_y = self.get_tile_cordinate(ti_x, ti_y)
         max_x, max_y = self.get_tile_cordinate(ti_x+1, ti_y+1)
@@ -275,42 +222,113 @@ class MainWidget(RelativeLayout):
                 return True
         return False
 
-    
-    def update(self, dt):
-        time_factor = dt*60
+    def update_vertical_lines(self):
+        start_index = -int(self.V_NB_LINES/2)+1
+        end_index = start_index + self.V_NB_LINES
+
+        for i in range(start_index, end_index):
+            line_x = self.get_line_x_from_index(i)
+
+            x1, y1 = self.transform(line_x, 0)
+            x2, y2 = self.transform(line_x, self.height)
+            self.vertical_lines[i].points = [x1,y1, x2,y2]
+
+    def update_horizontal_lines(self):
+        start_index = -int(self.V_NB_LINES/2) +1
+        end_index = start_index + self.V_NB_LINES-1
+        
+        min_x = self.get_line_x_from_index(start_index)
+        max_x = self.get_line_x_from_index(end_index)
+
+        for i in range(0, self.H_NB_LINES):
+            line_y = self.get_line_y_from_index(i) 
+
+            x1, y1 = self.transform(min_x, line_y)
+            x2, y2 = self.transform(max_x, line_y)
+            self.horizontal_lines[i].points = [x1,y1, x2,y2]
+
+    def update_tiles(self):
+        
+        for i in range(0, self.NB_TILES):
+            coordinates = self.tiles_coordinates[i]
+            min_x, min_y = self.get_tile_cordinate(coordinates[0], coordinates[1])
+            max_x, max_y = self.get_tile_cordinate(coordinates[0]+1, coordinates[1]+1)
+
+            x1, y1 = self.transform(min_x, min_y)
+            x2, y2 = self.transform(min_x, max_y)
+            x3, y3 = self.transform(max_x, max_y)
+            x4, y4 = self.transform(max_x, min_y)
+
+            self.tiles[i].points = [x1,y1, x2,y2, x3,y3, x4,y4]
+
+    def update_ship(self):
+        center_x = self.perspective_point_x
+        ship_width = self.SHIP_WIDTH*self.width
+
+        ship_height = self.SHIP_HEIGHT * self.height
+        ship_base_y = self.SHIP_BASE_Y * self.height
+
+        self.ship_coordinates[0] = (int(center_x - ship_width/2), ship_base_y)
+        self.ship_coordinates[1] = (center_x, ship_base_y + ship_height)
+        self.ship_coordinates[2] = (int(center_x + ship_width/2), ship_base_y)
+
+        x1, y1 = self.transform(*self.ship_coordinates[0])
+        x2, y2 = self.transform(*self.ship_coordinates[1])
+        x3, y3 = self.transform(*self.ship_coordinates[2])
+
+        self.ship.points = [x1,y1, x2,y2, x3,y3]
+
+    def update_graphics(self):
         self.update_vertical_lines()
         self.update_horizontal_lines()
         self.update_tiles()
         self.update_ship()
+ 
+    def vertical_move_board(self, time_factor):
+        speed_multiplier = 1 + self.current_y_loop / 100
+        speed_y = self.SPEED_Y * speed_multiplier * self.height / 100 
+        self.current_offset_y += speed_y * time_factor 
+    
+    def horizontal_move_board(self, time_factor):
+        speed_x = self.current_speed_x * self.width /100
+        self.current_offset_x +=  speed_x * time_factor
 
-        if self.game_started and not self.game_over:
-            if self.sound_music1.state == "stop":
-                self.sound_music1.play()
-            speed_multiplier = 1 + self.current_y_loop / 100
-            speed_y = self.SPEED_Y * speed_multiplier * self.height / 100 
-            self.current_offset_y += speed_y * time_factor 
+    def increment_y_loop(self, spacing_y):
+        self.current_offset_y -= spacing_y
+        self.current_y_loop += 1
+        self.score_text = "SCORE: " + str(self.current_y_loop)
+        self.generate_tiles_cordinates()
 
-            spacing_y = self.H_LINES_SPACING * self.height
-            while self.current_offset_y >= spacing_y:
-                self.current_offset_y -= spacing_y
-                self.current_y_loop += 1
-                self.score_text = "SCORE: " + str(self.current_y_loop)
-                self.generate_tiles_cordinates()
-            
-            speed_x = self.current_speed_x * self.width /100
-            self.current_offset_x +=  speed_x * time_factor
-
-        if not self.game_over and not self.check_ship_collision():
-            self.menu_widget.opacity = 1
-            self.game_over = True
-            self.sound_game_over_impact.play()
-            Clock.schedule_once(self.play_game_over_voice_sound, 1)
-            self.sound_music1.stop()
+    def run_game_over_process(self):
+        self.menu_widget.opacity = 1
+        self.game_over = True
+        self.sound_game_over_impact.play()
+        Clock.schedule_once(self.play_game_over_voice_sound, 1)
+        self.sound_music1.stop()
 
     def play_game_over_voice_sound(self, dt):
         if self.game_over:
             self.sound_game_over_voice.play()
-    
+        
+    def update(self, dt):
+        time_factor = dt*60
+        
+        self.update_graphics()
+
+        if self.game_started and not self.game_over:
+            if self.sound_music1.state == "stop":
+                self.sound_music1.play()
+            
+            self.vertical_move_board(time_factor)
+            self.horizontal_move_board(time_factor)
+
+            spacing_y = self.H_LINES_SPACING * self.height
+            while self.current_offset_y >= spacing_y:
+                self.increment_y_loop(spacing_y)
+
+        if not self.game_over and not self.check_ship_collision():
+            self.run_game_over_process()
+
     def on_menu_button_pressed(self):
         if not self.game_started:
             self.game_started = True
@@ -323,9 +341,6 @@ class MainWidget(RelativeLayout):
 
         self.sound_music1.play()
         self.menu_widget.opacity = 0
-        
-        
-
 
 class GalaxyApp(App):
     pass
